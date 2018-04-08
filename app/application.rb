bc9ea89627c133c77012c8661d2a9b78ebe887bf
paul.nicholsen <paul.nicholsen@flatironschool.com>
2018-04-08 11:33:29 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,22 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    num1 = get_random
+    num2 = get_random
+    num3 = get_random
+    resp.write "#{num1} #{num2} #{num3}"
+    resp.write "\n"
+    message = win?(num1, num2, num3) ? "You win" : "You lose"
+	resp.write message
     resp.finish
+  end
+
+  def win?(num1, num2, num3)
+  	num1 == num2 && num2 == num3
+  end
+
+  def get_random
+  	Kernel.rand(1..20)
   end
 
 end
